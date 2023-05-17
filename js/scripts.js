85c4cc26666d877dc3131c06c7ce9a55a858b76e
XiaFan <294259337tars@gmail.com>
2023-05-17 17:00:05 +0800

    update

--- js/scripts.js
+++ js/scripts.js
@@ -49,22 +49,6 @@ window.addEventListener('DOMContentLoaded', event => {
                 navbarToggler.click();
             }
         });
-
     });
-
-    // iframe height
-    function setIframeHeight(iframe) {
-    if (iframe) {
-    var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
-    if (iframeWin.document.body) {
-    iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
-    }
-    }
-    };
-    window.onload = function () {
-    setIframeHeight(document.getElementById('external-frame'));
-    };
-
-
 
 });
